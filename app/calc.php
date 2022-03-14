e616990fdabe029a52243c427181f549052c20d3
MikolajMus <mikomucha54@gmail.com>
2022-03-14 14:57:40 +0100

    l1

--- app/calc.php
+++ app/calc.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).'/../config.php';
 
 
 
-include _ROOT_PATH.'/app/Security/check.php';
+include _ROOT_PATH.'/app/security/check.php';
 //z-operacja
 
 function getParams(&$x,&$y,&$z){
