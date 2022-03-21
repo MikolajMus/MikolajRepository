529e5d4965c993b0610dfaf9af9110fd77724a08
MikolajMus <mikomucha54@gmail.com>
2022-03-21 11:55:36 +0100

    Zadanie z Smarty

--- app/calc.php
+++ app/calc.php
@@ -4,32 +4,34 @@ require_once dirname(__FILE__).'/../config.php';
 
 
 
-include _ROOT_PATH.'/app/security/check.php';
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
 //z-operacja
 
-function getParams(&$x,&$y,&$z){
-	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
-	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
-	$z = isset($_REQUEST['z']) ? $_REQUEST['z'] : null;
+function getParams(&$form){
+	$form['x'] = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$form['y'] = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$form['z'] = isset($_REQUEST['z']) ? $_REQUEST['z'] : null;
 }
 
 
 
-function validate(&$x,&$y,&$z,&$messages){
+function validate(&$form,&$infos,&$hide_intro,&$messages){
 
-if ( ! (isset($x) && isset($y)  && isset($z))) {
+if ( ! (isset($form['x']) && isset($form['y'])  && isset($form['z'])))
 	return false;
 
-}
+$hide_intro = true;
+
+$infos [] = 'Przekazano parametry.';
 
 // sprawdzenie, czy wartosci zostały podane
-if ( $x == "") {
+if ( $form['x'] == "") {
 	$messages [] = 'Nie podano kwoty';
 }
-if ( $y == "") {
+if ( $form['y'] == "") {
 	$messages [] = 'Nie podano lat kredytu';
 }
-if ( $z == "") {
+if ( $form['z'] == "") {
 	$messages [] = 'Nie podano oprocentowania';
 }
 
@@ -37,14 +39,14 @@ if ( $z == "") {
 if (count( $messages ) != 0) return false;
 
 	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-    if (! is_numeric( $x )||!$x>0) {
+    if (! is_numeric( $form['x'] )||!$form['x']>0) {
 		     $messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
 	}
 
-    if (! is_numeric( $y )||!$y>0) {
+    if (! is_numeric( $form['y'] )||!$form['y']>0) {
 		     $messages [] = 'Druga wartość nie jest liczbą większą od zera';
 	}
-    if (! is_numeric( $z )||!$z>0) {
+    if (! is_numeric( $form['z'] )||!$form['z']>0) {
 		     $messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
 	}
 		if (count($messages) != 0) return false;
@@ -52,29 +54,44 @@ if (count( $messages ) != 0) return false;
 }
 
 
-function process($x,$y,$z,&$messages,&$result){
+function process(&$form,&$infos,&$messages,&$result){
 global $role;
 	//konwersja parametrów na int
-	      $x = intval($x);
-	      $y = intval($y);
-	      $z = intval($z);
+	      $form['x'] = intval($form['x']);
+	      $form['y'] = intval($form['y']);
+	      $form['z'] = intval($form['z']);
 
 
 	//wykonanie operacji
-	if ($role != 'admin' &&$x>2000){
+	if ($role != 'admin' &&$form['x']>2000){
 		$messages[] = 'Tylko admin moze brac kredyt na wiecej niz 2000';
 }
  else{
-	$z=12*$z;
-	$y=$y/100;
-	$result = ($x*$y)/(12*(1-((12/(12+$y))**$z)));
-}
+	$form['z']=12*$form['z'];
+	$form['y']=$form['y']/100;
+	$result = ($form['x']*$form['y'])/(12*(1-((12/(12+$form['y']))**$form['z'])));
+	}
 }
 $messages = array();
-getParams($x,$y,$z);
-	if ( validate($x,$y,$z,$messages) ) {
- process($x,$y,$z,$messages,$result);
+getParams($form);
+	if ( validate($form,$infos,$hide_intro,$messages) ) {
+ process($form,$infos,$messages,$result);
 
 }
-include 'calc_view.php';
-?>
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Przykład 04');
+$smarty->assign('page_description','Profesjonalne szablonowanie oparte na bibliotece Smarty');
+$smarty->assign('page_header','Szablony Smarty');
+
+
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+
+$smarty->display(_ROOT_PATH.'/app/calc.html');
